0852e013ed6409935ec532ea9cdcdd75cb975a8a
Steven Ortega <stevencortega99@gmail.com>
2023-04-18 15:13:07 -0500

    Adding working files from other repo that I messed up

--- rg_utils/scripts/load_vectormap.py
+++ rg_utils/scripts/load_vectormap.py
@@ -13,8 +13,6 @@ from geometry_msgs.msg import PoseStamped, Pose
 
 
 _FRAME_ID = 'map'
-_FILENAME = '/home/knapsack/workspaces/test_ws/src/mrsearch/tmp/vectormaps/ahg-alley-sim/ahg-alley-sim.vectormap.txt'
-
 
 def vectormap_manager():    
     msg = Path()
@@ -23,25 +21,30 @@ def vectormap_manager():
 
     vectormap_pub=rospy.Publisher('/vectormap',Path,queue_size=10)
 
-    map_txt = open(_FILENAME, 'r')
+    map_txt = open(_FILEPATH, 'r')
 
     for line in map_txt:
-        pose = PoseStamped()
-        # line = map_txt.readline()
-        # if not line:
-            # print('break')
-            # break
-        values = line.split(',')
-        # print(values[0])
-        # print(values[1])
-        pose.pose.position.x = float(values[0])
-        pose.pose.position.y = float(values[1])
-        pose.pose.position.z = 0.0
-        pose.pose.orientation.x = 0.
-        pose.pose.orientation.y = 0.
-        pose.pose.orientation.z = 0.
-        pose.pose.orientation.w = 1.
-        msg.poses.append(pose)
+        if line == '\n':
+            pass
+        else:
+            pose = PoseStamped()
+            # line = map_txt.readline()
+            # if not line:
+                # print('break')
+                # break
+            values = line.split(',')
+            # print(values[0])
+            # print(values[1])
+            pose.pose.position.x = float(values[0])
+            pose.pose.position.y = float(values[1])
+            pose.pose.position.z = 0.0
+            pose.pose.orientation.x = 0.
+            pose.pose.orientation.y = 0.
+            pose.pose.orientation.z = 0.
+            pose.pose.orientation.w = 1.
+            msg.poses.append(pose)
+            _ = raw_input('next')
+            vectormap_pub.publish(msg)
 
     map_txt.close()
     rospy.loginfo("Publishing Vectormap...")
@@ -53,6 +56,7 @@ def vectormap_manager():
 
 
 if __name__ == '__main__':
+    _FILEPATH = raw_input('Filepath to vector map: ')
     rospy.init_node('vectormap_manager')
     vectormap_manager()
-    # mains()
+   # mains()
