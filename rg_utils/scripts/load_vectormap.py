998b063496dce2dda5732f7d9cc9180818c4ddc9
Steven Ortega <stevencortega99@gmail.com>
2023-05-03 19:05:19 -0500

    updated load vectormap script and added testzone, workshop

--- rg_utils/scripts/load_vectormap.py
+++ rg_utils/scripts/load_vectormap.py
@@ -13,8 +13,6 @@ from geometry_msgs.msg import PoseStamped, Pose
 
 
 _FRAME_ID = 'map'
-_FILENAME = '/home/knapsack/workspaces/test_ws/src/mrsearch/tmp/vectormaps/ahg-alley-sim/ahg-alley-sim.vectormap.txt'
-
 
 def vectormap_manager():    
     msg = Path()
@@ -23,36 +21,42 @@ def vectormap_manager():
 
     vectormap_pub=rospy.Publisher('/vectormap',Path,queue_size=10)
 
-    map_txt = open(_FILENAME, 'r')
+    flag = 0
+    while not rospy.is_shutdown() and not flag:
 
-    for line in map_txt:
-        pose = PoseStamped()
-        # line = map_txt.readline()
-        # if not line:
-            # print('break')
-            # break
-        values = line.split(',')
-        # print(values[0])
-        # print(values[1])
-        pose.pose.position.x = float(values[0])
-        pose.pose.position.y = float(values[1])
-        pose.pose.position.z = 0.0
-        pose.pose.orientation.x = 0.
-        pose.pose.orientation.y = 0.
-        pose.pose.orientation.z = 0.
-        pose.pose.orientation.w = 1.
-        msg.poses.append(pose)
-
-    map_txt.close()
-    rospy.loginfo("Publishing Vectormap...")
-
-    while not rospy.is_shutdown():
-        vectormap_pub.publish(msg)
-
+        map_txt = open(_FILEPATH, 'r')
+        for line in map_txt:
+            if line == '\n':
+                pass
+            else:
+                pose = PoseStamped()
+                # line = map_txt.readline()
+                # if not line:
+                    # print('break')
+                    # break
+                values = line.split(',')
+                # print(values[0])
+                # print(values[1])
+                pose.pose.position.x = float(values[0])
+                pose.pose.position.y = float(values[1])
+                pose.pose.position.z = 0.0
+                pose.pose.orientation.x = 0.
+                pose.pose.orientation.y = 0.
+                pose.pose.orientation.z = 0.
+                pose.pose.orientation.w = 1.
+                msg.poses.append(pose)
+                z = raw_input('next?')
+                if z == "q":
+                    rospy.signal_shutdown()
+                    flag = 1
+                vectormap_pub.publish(msg)
+        map_txt.close()
+        flag = 1
 
 
 
 if __name__ == '__main__':
+    _FILEPATH = raw_input('Filepath to vector map: ')
     rospy.init_node('vectormap_manager')
     vectormap_manager()
-    # mains()
+   # mains()
